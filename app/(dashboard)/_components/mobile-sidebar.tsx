40fe485b43abb0c1449436e1861159ae78acdad0
Sahil Sundrani <sahilsundrani@Sahils-MacBook-Air.local>
2024-04-13 17:06:24 +0530

    basic layout completed for teacher and student mode

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,23 @@
+import { Menu  } from "lucide-react";
+import { 
+    Sheet, 
+    SheetContent, 
+    SheetTrigger 
+} from "@/components/ui/sheet";
+import { Sidebar } from "./sidebar";
+
+export const MobileSidebar = () => {
+    return (
+        <div>
+            <Sheet>
+                <SheetTrigger className="md:hidden pr-4 hover:opacity-45 transition">
+                    <Menu /> 
+                </SheetTrigger>
+                <SheetContent side="left" className="p-0 bg-white">
+                    <Sidebar />
+                </SheetContent>  
+            </Sheet>
+ 
+        </div>
+    )
+}
