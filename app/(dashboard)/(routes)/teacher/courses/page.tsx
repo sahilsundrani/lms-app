a79c1143f6aff7a351eb168ded409a0a32516737
Sahil Sundrani <sahilsundrani@Sahils-MacBook-Air.local>
2024-04-23 20:06:38 +0530

    added data table for course

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,16 +1,36 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns"; 
+
+const CoursesPage = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId, 
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
+
+     
     return (
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>
-                    New Course
-                </Button>
-            </Link>
+            <DataTable
+                columns={columns}
+                data={courses}
+            />
         </div>
     );
 }
  
-export default CoursesPage;
+export default CoursesPage;  
